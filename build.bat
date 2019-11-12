62edfd6211dd150046bd65e7f6ae059526ded60c
Liam Swarbrick <liam.swarbrick2004@gmail.com>
2019-11-12 21:28:42 +0000

    removed message related to building another project

--- build.bat
+++ build.bat
@@ -1,7 +1,5 @@
 @echo off
 
-REM maybe in future use dll and have application code load it at runtime
-
 SET COMMON_FLAGS=-Wall -Wextra -Wno-unused-variable -Wno-unused-parameter -Wno-missing-braces -std=c99
 
 PUSHD src
